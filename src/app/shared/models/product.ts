71695758a255c48852dea9020bfc064fa456c84c
chien_nguyen <chienminhnguyen196@gmal.com>
2018-07-01 18:55:29 +0700

    add some models

---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    brand: string;
+    colors: string[];
+    count: number;
+    description: string;
+    id: string;
+    name: string;
+    price: number;
+    price_unit: string;
+    sizes: number[];
+    url: string;
+    url_images: string[];
+}
