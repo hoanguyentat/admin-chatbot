137e3f23926d3655c69f7f45fc389a6d97702c05
iot-chiennm <iot-chiennm@ViettelGroup.ViettelAD.com>
2018-07-02 18:32:27 +0700

    add product list

---
+++ src/app/admin/shop-management/product-management/product-management.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-management',
+  template: '<router-outlet></router-outlet>'
+})
+export class ProductManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/admin/shop-management/product-management/product-management.module.ts
+++ src/app/admin/shop-management/product-management/product-management.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductManagementComponent } from './product-management.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { Routes, RouterModule } from '@angular/router';
+
+const route: Routes = [{
+  path: '',
+  component: ProductManagementComponent,
+  children: [
+    {
+      path: 'list', component: ProductListComponent
+    },     {
+      path: '', redirectTo: 'list'
+    }, {
+      path: 'detail/:id', component: ProductDetailComponent
+    }
+  ]
+}]
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    RouterModule.forChild(route)
   ],
-  declarations: []
+  declarations: [ProductListComponent, ProductDetailComponent, ProductManagementComponent]
 })
 export class ProductManagementModule { }
