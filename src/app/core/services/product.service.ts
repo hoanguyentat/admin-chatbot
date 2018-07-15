ca54f877a96e449377604cf146984644d023ee46
chien_nguyen <chienminhnguyen196@gmal.com>
2018-07-15 21:56:30 +0700

    add message service

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -3,6 +3,8 @@ import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { MessageService } from './message.service';
+import { tap, catchError } from '../../../../node_modules/rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,10 @@ export class ProductService {
 
   private baseUrl = environment.domain + '/api/v1/products/';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private message: MessageService
+  ) { }
 
   private getUrl(id?) {
     if (id) {
@@ -22,7 +27,13 @@ export class ProductService {
   }
 
   public createProduct(product: Product): Observable<any> {
-    return this.http.post(this.getUrl(), JSON.stringify(product));
+    return this.http.post(this.getUrl(), JSON.stringify(product)).pipe(
+      tap(_ => this.message.info('Create product successfully')),
+      catchError((err) => {
+        this.message.error('Create product fail!');
+        throw err;
+      })
+    );
   }
 
   public getProducts(page: number = 1, page_size: number = 10): Observable<any> {
@@ -35,11 +46,25 @@ export class ProductService {
 
   public updateProduct(product: Product): Observable<any> {
     const _url = this.getUrl(product.id);
-    return this.http.put(_url, JSON.stringify(product));
+    return this.http.put(_url, JSON.stringify(product)).pipe(
+      tap(_ => {
+        this.message.info('Update product successfully');
+      }),
+      catchError((err) => {
+        this.message.error('Update product fail!');
+        throw err;
+      })
+    );
   }
 
   public deleteProduct(productId: number | string): Observable<any> {
     const _url = this.getUrl(productId);
-    return this.http.delete(_url);
+    return this.http.delete(_url).pipe(
+      tap(_ => this.message.info('Delete product successfully')),
+      catchError((err) => {
+        this.message.error('Delete product fail!');
+        throw err;
+      })
+    );
   }
 }
