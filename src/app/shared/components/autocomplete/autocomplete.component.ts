dea3a87fab0ec0601c3d942ad42d2df6d679957a
chien_nguyen <chienminhnguyen196@gmal.com>
2018-07-17 00:23:57 +0700

    fix not fill value into autocomplet

--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subject, merge } from 'rxjs';
 import { startWith, map, tap } from 'rxjs/operators';
 
 @Component({
@@ -8,7 +8,7 @@ import { startWith, map, tap } from 'rxjs/operators';
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.scss']
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent implements OnInit, OnChanges {
   myControl: FormControl;
   filteredOptions: Observable<string[]>;
   @Input()
@@ -22,20 +22,33 @@ export class AutocompleteComponent implements OnInit {
   @Output()
   modelChange: EventEmitter<string>;
 
+  private changeSubject: Subject<string>;
+
   constructor() {
     this.myControl = new FormControl();
     this.modelChange = new EventEmitter<string>();
+    this.changeSubject = new Subject<string>();
   }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges
+    this.filteredOptions = merge(this.myControl.valueChanges, this.changeSubject)
       .pipe(
         startWith(this.model || ''),
         tap(value => {
+          console.log(value);
           this.modelChange.emit(value);
+          this.myControl.setValue(value, {
+            emitEvent: false,
+            emitModelToViewChange: true,
+          });
         }),
         map(value => this._filter(value)),
-      );
+    );
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes['model'].currentValue);
+    this.changeSubject.next(this.model);
   }
 
   private _filter(value: string): string[] {
